31e539efafa35e34d3defcdd2a151d425fde0086
TAKADA-yuma <89758513+TAKADA-yuma@users.noreply.github.com>
2022-12-24 14:55:09 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: %i(index new create)
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index]
   before_action :move_to_index, only: %i(edit update destroy)
 
 
